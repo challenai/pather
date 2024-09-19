d7527aa3d191ca2939bc68c31c20840f82a3ef2b
ChallenAi <fengyiai@gmail.com>
2024-09-19 05:29:45 +0000

    feat: translate the circle to its geometric center

--- lib/circle.ts
+++ lib/circle.ts
@@ -1,7 +1,7 @@
 export class Circle {
   static Basic(x: number, y: number, radius: number): string {
     return `
-      M${x} ${y}
+      M${x + radius} ${y}
       a${radius} ${radius} 0 1 0 ${-radius * 2} 0
       a${radius} ${radius} 0 1 0 ${radius * 2} 0
     `;
