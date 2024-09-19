4451856271567d57b1620f32ba1500f11bd27287
ChallenAi <fengyiai@gmail.com>
2024-09-19 04:09:35 +0000

    feat: impl rectangle with round coner

--- src/rectangle.ts
+++ src/rectangle.ts
@@ -1,3 +1,10 @@
+export interface Radius4 {
+  tl: number;
+  tr: number;
+  bl: number;
+  br: number;
+}
+
 export class Rectangle {
   static Basic(x: number, y: number, width: number, height: number): string {
     return `
@@ -5,6 +12,36 @@ export class Rectangle {
       L${x + width} ${y}
       L${x + width} ${y + height}
       L${x} ${y + height}
+      Z
+    `;
+  }
+
+  static Round(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    radius: number | Radius4
+  ): string {
+    if (typeof radius === "number") {
+      radius = { tl: radius, tr: radius, br: radius, bl: radius };
+    }
+
+    if (!radius.tl) radius.tl = 0;
+    if (!radius.tr) radius.tr = 0;
+    if (!radius.bl) radius.bl = 0;
+    if (!radius.br) radius.br = 0;
+
+    return `
+      M${x + radius.tl} ${y}
+      L${x + width - radius.tr} ${y}
+      a${radius.tr} ${radius.tr} 0 0 1 ${radius.tr} ${radius.tr}
+      L${x + width} ${y + height - radius.br}
+      a${radius.br} ${radius.br} 0 0 1 ${-radius.br} ${radius.br}
+      L${x + radius.bl} ${y + height}
+      a${radius.bl} ${radius.bl} 0 0 1 ${-radius.bl} ${-radius.bl}
+      L${x} ${y + radius.tl}
+      a${radius.tl} ${radius.tl} 0 0 1 ${radius.tl} ${-radius.tl}
       Z
     `;
   }
