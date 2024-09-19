2d9489c94a0687f1e94c88847cb9078c8673ef1b
ChallenAi <fengyiai@gmail.com>
2024-09-19 04:18:08 +0000

    feat: impl fold line

--- src/line.ts
+++ src/line.ts
@@ -2,4 +2,15 @@ export class Line {
   static Basic(x: number, y: number, x1: number, y1: number): string {
     return `M${x} ${y} L${x1} ${y1}`;
   }
+
+  static Fold(x: number, y: number, points: number[][]): string {
+    if (!points) return "";
+
+    let path = `M${x} ${y}`;
+    for (const point of points) {
+      path += ` L${point[0]} ${point[1]}`;
+    }
+
+    return path;
+  }
 }
