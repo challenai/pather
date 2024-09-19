4d4aa6ce0d9d1ca2023527cb9e3df4cdc95f9aa6
ChallenAi <fengyiai@gmail.com>
2024-09-19 03:54:50 +0000

    feat: impl basic circle

---
+++ src/circle.ts
@@ -0,0 +1,9 @@
+export class Circle {
+  static Simple(x: number, y: number, radius: number): string {
+    return `
+      M${x} ${y}
+      a${radius} ${radius} 0 1 0 ${-radius * 2} 0
+      a${radius} ${radius} 0 1 0 ${radius * 2} 0
+    `;
+  }
+}
