b6f9aef1c73c085ffc3ae819f84f1dee1f995ce7
ChallenAi <fengyiai@gmail.com>
2024-09-19 03:56:24 +0000

    feat: impl basic step line

---
+++ src/step.ts
@@ -0,0 +1,19 @@
+export class Step {
+  // points example: [[0, 220], [1, 120]]
+  // 0: x, 1: y
+  static Basic(x: number, y: number, points: number[][]): string {
+    if (!points) return "";
+
+    let path = `M${x} ${y}`;
+    for (const point of points) {
+      if (point[0] === 0) {
+        x = point[1];
+      } else {
+        y = point[1];
+      }
+      path += ` L${x} ${y}`;
+    }
+
+    return path;
+  }
+}
