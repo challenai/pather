96ec9fa1640cd6fb6f35ec8c9aeee9e1f9b3595b
ChallenAi <fengyiai@gmail.com>
2024-09-19 05:39:31 +0000

    feat: translate the capsule to its geometric center, use relative position to simplify

--- lib/capsule.ts
+++ lib/capsule.ts
@@ -1,14 +1,13 @@
 export class Capsule {
   static Row(x: number, y: number, width: number, height: number): string {
-    if (width <= height) return "";
+    if (width < height) return "";
 
     const radius = height / 2;
-    const innerWidth = width - height;
     return `
-      M${x} ${y}
-      L${x + innerWidth} ${y}
+      M${x - radius} ${y - radius}
+      l${width - height} 0
       a${radius} ${radius} 0 0 1 0 ${height}
-      L${x} ${y + height}
+      l${height - width} 0
       a${radius} ${radius} 0 0 1 0 ${-height}
     `;
   }
