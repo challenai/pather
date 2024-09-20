c1a25d62cbad464aeeea9c8fcb77c382d0725e76
ChallenAi <fengyiai@gmail.com>
2024-09-20 13:44:26 +0000

    refactor: remove multi control points curve entry point

--- lib/curve.ts
+++ lib/curve.ts
@@ -12,23 +12,18 @@ export class Curve {
 
   // curve with multi control point
   static Multi(
-    x: number,
-    y: number,
     points: number[][],
     radius: number
   ): string {
-    if (!points) return "";
+    if (!points || points.length === 1) return "";
     const sz = points.length;
-    if (sz === 1) {
-      return `M${x} ${y} L${points[0][0]} ${points[0][1]}`;
-    }
 
-    let path = `M${x} ${y}`;
-    for (let i = 0; i < sz - 1; i++) {
+    let path = `M${points[0][0]} ${points[0][1]}`;
+    for (let i = 1; i < sz - 1; i++) {
       const x_ = points[i][0];
       const y_ = points[i][1];
-      const deltaX1 = i === 0 ? x_ - x : x_ - points[i - 1][0];
-      const deltaY1 = i === 0 ? y_ - y : y_ - points[i - 1][1];
+      const deltaX1 = x_ - points[i - 1][0];
+      const deltaY1 = y_ - points[i - 1][1];
       const deltaX2 = x_ - points[i + 1][0];
       const deltaY2 = y_ - points[i + 1][1];
       const ratio = radius / Math.sqrt(deltaX2 * deltaX2 + deltaY2 * deltaY2);
