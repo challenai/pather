ed8198376d8ca23326892bf4e7866c084cdff259
ChallenAi <fengyiai@gmail.com>
2024-09-20 12:49:35 +0000

    feat: impl regular polygon

--- lib/polygon.ts
+++ lib/polygon.ts
@@ -12,4 +12,16 @@ export class Polygon {
 
     return `M${x - offsetX / n} ${y - offsetY / n} ${path} Z`;
   }
+
+  static Regular(x: number, y: number, sides: number, radius: number): string {
+    const base = 2 * Math.PI / sides;
+    let path = ""
+    for (let i = 1; i < sides; i++) {
+      const radians = base * i;
+      path += ` l${radius * Math.cos(radians)} ${radius * Math.sin(radians)}`
+    }
+
+    // FIX: align to its center
+    return `M${x + radius / 2} ${y} ${path} Z`;
+  }
 }
